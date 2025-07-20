6c80751cdb59b75241fcc38be6380d8e76745542
Dmitry Andreev <andreev.dp@bk.ru>
2025-07-20 23:52:43 +0400

    feat: openapi

---
+++ api/generate.go
@@ -0,0 +1,5 @@
+package api
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=configs/types.yaml openapi.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=configs/server.yaml openapi.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=configs/client.yaml openapi.yaml
